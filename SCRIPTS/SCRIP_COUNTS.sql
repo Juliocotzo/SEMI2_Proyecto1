6a78701629664c7b3e8c9ac348575df22513b1c1
Julio Cotzojay <juliocotzo11@gmail.com>
2020-06-14 02:31:25 -0600

    DOCUMENTACION

---
+++ SCRIPTS/SCRIP_COUNTS.sql
@@ -0,0 +1,16 @@
+USE seminario201504381
+GO
+SELECT COUNT(*) AS articulo FROM articulo
+GO
+SELECT COUNT(*) AS cliente FROM cliente
+GO
+SELECT COUNT(*) AS fecha FROM fecha
+GO
+SELECT COUNT(*) AS region FROM region
+GO
+SELECT COUNT(*) AS sucursal FROM sucursal
+GO
+SELECT COUNT(*) AS vendedor FROM vendedor
+GO
+SELECT COUNT(*) AS venta FROM venta
+GO
